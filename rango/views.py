147ab22d4b8d1cd242db5724175119e57d7e73dd
mMarzeta <94maciekm@gmail.com>
2016-11-18 00:31:16 +0100

    ADD Adding Category

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     category_views = Category.objects.order_by('-views')[:5]
-    context_dict  = {'categories_likes' : category_list, 'categories_views' : category_views}
+    categories_all = Category.objects.all()
+    context_dict  = {'categories_likes' : category_list, 'categories_views' : category_views,
+                     'categories_all' : categories_all}
     return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
@@ -27,3 +30,18 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print (form.errors)
+
+    return render(request, 'rango/add_category.html', {'form' : form})
+
